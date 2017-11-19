4e5429fb1232d100533151f89f83c51e01e2f8fb
toot-toot <melissa.zhang98@gmail.com>
2017-11-19 11:25:56 +1100

    adding shit

--- mTutes/lec/w4.cpp
+++ mTutes/lec/w4.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 
+// function object
 class Range{
 public:
    Range(int l, int u) : lb(l), ub(u) { }
@@ -13,6 +14,15 @@ private:
    int lb, ub;
 };
 
+// template function object
+template <class T, T lb, T ub>
+struct Range2 {
+   bool operator() (T val) {
+      return (val >= lb && val <= ub);
+   }
+};
+
+// function
 bool range5to10(int val) {
    return (val >= 5 && val <= 10);
 }
@@ -20,20 +30,30 @@ bool range5to10(int val) {
 // function pointer
 bool (*pf)(int) = range5to10;
 
+void f(int a, int b, int c) {
+   std::cout << a << " " << b << " " << c << "\n";
+}
+
 int main() {
    std::vector<int> vec{ 20, 2, 4, 3, 8, 10, 15, 1};
 
-   auto range = [](auto a){ return (a >= 5 && a <= 10); };
+   auto range = [](auto &a){ return (a >= 5 && a <= 10); };
 
    auto result = std::find_if(vec.begin(), vec.end(), Range{5, 10});   
    auto result2 = std::find_if(vec.begin(), vec.end(), range5to10);
    auto result3 = std::find_if(vec.begin(), vec.end(), pf);
    auto result4 = std::find_if(vec.begin(), vec.end(), range);
+   auto result5 = std::find_if(vec.begin(), vec.end(), Range2<int, 5, 10>{});
 
    if (result != vec.end()) {
       std::cout << *result << "\n";
       std::cout << *result2 << "\n";
       std::cout << *result3 << "\n";
       std::cout << *result4 << "\n";
+      std::cout << *result5 << "\n";
    }
+
+   // bind switches order of function args
+   auto g1 = std::bind(f, std::placeholders::_2, std::placeholders::_1, 100);
+   g1(3, 1); // prints 1 3 100
 }
--- mTutes/t6/variadicPrint.cpp
+++ mTutes/t6/variadicPrint.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+
 template <typename T>
 bool printIfWholeNumber(const T&) {
    return false;
@@ -8,3 +11,65 @@ bool printIfWholeNumber<int>(const int& i) {
    std::cout << i;
    return true;
 }
+
+// What will this template do if it is given a std::string, a double, or a EuclideanVector?
+// return false
+// What will this template do if it is given an int?
+// print int, return true
+// What will this template do if it is given a long?
+// return false
+
+template <typename T>
+unsigned int printAndCountWholeNumbers(const T& d) {
+   if (printIfWholeNumber(d)) {
+      std::cout << " ";
+      return 1;
+   }
+   return 0;
+}
+
+// What will this template do if it is given a std::string, a double, or a EuclideanVector?
+// return false
+// What will this template do if it is given an int?
+// return true, print int + ' '
+// What will this template do if it is given a long?
+// return false
+
+template <typename T, typename... U>
+unsigned int printAndCountWholeNumbers(T head, U... tail) {
+   // call a function to work out if the "head" is a whole number
+   bool wHead = printAndCountWholeNumbers(head);
+   // call a function to process the tail
+   int wTail = printAndCountWholeNumbers(tail...);
+   // return the number of whole numbers in the variadic parameters
+   return wHead + wTail;
+}
+
+int main() {
+   // double a = 3.0;
+   // long b = 2132;
+   // int c = 1;
+   // printIfWholeNumber(a);
+   // printIfWholeNumber(b);
+   // printIfWholeNumber(c);
+   // printAndCountWholeNumbers(a);
+
+   auto c = printAndCountWholeNumbers(1, 2.5, 3, 4.5, 5.5, 6, 7.0, -5, "2" );
+   std::cout << "count = " << c << std::endl;
+   // The numbers 1 3 6 -5 should be printed out. Why is the number 7 not printed? Why is the number 2 not printed? 7.0 is a double, "2" is a string
+
+   std::vector<double> d = {1.2, 32.0, 3.2, 5.30, 5.4, 6, -5};
+   auto dc = printAndCountWholeNumbers(d);
+   std::cout << "count = " << dc << std::endl;
+
+   std::vector<unsigned int> vui = {65, 343, 21, 3};
+   dc = printAndCountWholeNumbers(vui);
+   std::cout << "count = " << dc << std::endl;
+   
+   // This code will compile and run, but it won't print out the correct number of whole numbers in either vector. Why?
+
+
+}
+
+
+
