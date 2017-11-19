49fff9ceeb9f72af674d387c40061bef4dc89555
toot-toot <melissa.zhang98@gmail.com>
2017-11-19 13:08:22 +1100

    addddddding w6

--- mTutes/lec/w6.cpp
+++ mTutes/lec/w6.cpp
@@ -1,2 +1,14 @@
-const a;         // value can't change
-constexpr b = 1; // evaluated at compile time
+//const a;         // value can't change
+// constexpr b = 1; // evaluated at compile time
+
+#include <iostream>
+
+template <typename T>
+T min(T a, T b) { return a < b ? a : b; }
+
+int main() {
+   const char *s = "zyx";
+   const char *t = "abc";
+   std::cout << min(s,t) << std::endl;
+   // prints zyx
+}
